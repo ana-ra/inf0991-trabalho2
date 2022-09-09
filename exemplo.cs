c490135dcaf3e4c29f82b19a736d16b4b441f51f
ana-ra <silvanarodriguesalvess@gmail.com>
2022-09-08 22:26:15 -0300

    Trabalho 2

---
+++ exemplo.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        var resultado = (6 + 8) / 2;
+        Console.Write("Media");
+        Console.WriteLine(resultado);
+
+    }
+}
